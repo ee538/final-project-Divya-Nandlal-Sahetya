b53de7bc2d4553e663a1027d3de128e17c89359f
Divya Nandlal Sahetya <sahetya@usc.edu>
2022-04-20 22:46:45 -0700

    Phase 2 testcase

--- tests/trojanmap_test_student.cc
+++ tests/trojanmap_test_student.cc
@@ -1,8 +1,9 @@
 #include "gtest/gtest.h"
 #include "src/lib/trojanmap.h"
 
+// Phase 1
 // Autocomplete function
-TEST(TrojanMapTest, Autocomplete) {
+TEST(TrojanMapStudentTest, Autocomplete) {
   TrojanMap m;
   // Test the simple case
   auto names = m.Autocomplete("Sh");
@@ -103,7 +104,7 @@ TEST(TrojanMapStudentTest, FindPosition) {
 }
 
 // Test CalculateEditDistance function
-TEST(TrojanMapTest, CalculateEditDistance) {
+TEST(TrojanMapStudentTest, CalculateEditDistance) {
   TrojanMap m;
   EXPECT_EQ(m.CalculateEditDistance("horse", "ros"), 3);
   EXPECT_EQ(m.CalculateEditDistance("cat", "cut"), 1);
@@ -111,7 +112,7 @@ TEST(TrojanMapTest, CalculateEditDistance) {
 }
 
 // Test FindClosestName function
-TEST(TrojanMapTest, FindClosestName) {
+TEST(TrojanMapStudentTest, FindClosestName) {
   TrojanMap m;
   // Example 1
   // Test the same case
@@ -131,4 +132,100 @@ TEST(TrojanMapTest, FindClosestName) {
   // Example 4
   std::string r4 = "Just Ride LA";
   EXPECT_EQ(m.FindClosestName("JustRideLA"), r4);
+}
+
+// Phase 2
+// Test CalculateShortestPath_Dijkstra function
+TEST(TrojanMapStudentTest, CalculateShortestPath_Dijkstra) {
+  TrojanMap m;
+  
+  // Test from KFC to Ralphs
+  auto path = m.CalculateShortestPath_Dijkstra("KFC", "Ralphs");
+  std::vector<std::string> gt{"3088547686","4835551100","4835551097","4835551101","4835551096","2613117890","6807554573","6787803640","7298150111","7477947679","6813416163","122814440","7200139036","7882624618","6813416166","6807536642","6807320427","6807536647","6813416171","6813416123","3398621888","6804883324","5690152756","6816193695","3398621887","3398621886","6816193694","6816193693","6816193692","4015442011","6787470576","6816193770","123230412","452688931","452688933","6816193774","123408705","6816193777","452688940","123318563","6813416129","6813416130","7645318201","6813416131","8410938469","6805802087","4380040167","4380040158","4380040154","2578244375"}; // Expected path
+  // Print the path lengths
+  std::cout << "My path length: "  << m.CalculatePathLength(path) << "miles" << std::endl;
+  std::cout << "GT path length: " << m.CalculatePathLength(gt) << "miles" << std::endl;
+  EXPECT_EQ(path, gt);
+  
+  // Reverse the input from Ralphs to KFC
+  path = m.CalculateShortestPath_Dijkstra("Ralphs", "KFC");
+  std::reverse(gt.begin(),gt.end()); // Reverse the path
+
+  // Print the path lengths
+  std::cout << "My path length: "  << m.CalculatePathLength(path) << "miles" << std::endl;
+  std::cout << "GT path length: " << m.CalculatePathLength(gt) << "miles" << std::endl;
+  EXPECT_EQ(path, gt);
+}
+
+// Test CalculateShortestPath_Bellman_Ford function
+TEST(TrojanMapStudentTest, CalculateShortestPath_Bellman_Ford) {
+  TrojanMap m;
+  
+  // Test from KFC to Ralphs
+  auto path = m.CalculateShortestPath_Bellman_Ford("KFC", "Ralphs");
+  std::vector<std::string> gt{
+      "3088547686","4835551100","4835551097","4835551101","4835551096","2613117890","6807554573","6787803640","7298150111","7477947679","6813416163","122814440","7200139036","7882624618","6813416166","6807536642","6807320427","6807536647","6813416171","6813416123","3398621888","6804883324","5690152756","6816193695","3398621887","3398621886","6816193694","6816193693","6816193692","4015442011","6787470576","6816193770","123230412","452688931","452688933","6816193774","123408705","6816193777","452688940","123318563","6813416129","6813416130","7645318201","6813416131","8410938469","6805802087","4380040167","4380040158","4380040154","2578244375"}; // Expected path
+  // Print the path lengths
+  std::cout << "My path length: "  << m.CalculatePathLength(path) << "miles" << std::endl;
+  std::cout << "GT path length: " << m.CalculatePathLength(gt) << "miles" << std::endl;
+  EXPECT_EQ(path, gt);
+  
+  // Reverse the input from Ralphs to KFC
+  path = m.CalculateShortestPath_Bellman_Ford("Ralphs", "KFC");
+  std::reverse(gt.begin(),gt.end()); // Reverse the path
+
+  // Print the path lengths
+  std::cout << "My path length: "  << m.CalculatePathLength(path) << "miles" << std::endl;
+  std::cout << "GT path length: " << m.CalculatePathLength(gt) << "miles" << std::endl;
+  EXPECT_EQ(path, gt);
+}
+
+// Test cycle detection function
+TEST(TrojanMapStudentTest, CycleDetection) {
+  TrojanMap m;
+  
+  // Test case 1
+  std::vector<double> square1 = {-118.299, -118.264, 34.032, 34.011};
+  auto sub1 = m.GetSubgraph(square1);
+  bool result1 = m.CycleDetection(sub1, square1);
+  EXPECT_EQ(result1, true);
+
+  // Test case 2
+  std::vector<double> square2 = {-118.290, -118.289, 34.030, 34.020};
+  auto sub2 = m.GetSubgraph(square2);
+  bool result2 = m.CycleDetection(sub2, square2);
+  EXPECT_EQ(result2, false);
+
+  // Test case 3
+  std::vector<double> square3= {-118.320, -118.290, 34.030, 34.010};
+  auto sub3 = m.GetSubgraph(square3);
+  bool result3 = m.CycleDetection(sub3, square3);
+  EXPECT_EQ(result3, true);
+
+  // Test case 4
+  std::vector<double> square4 = {-118.290, -118.289, 34.030, 34.030};
+  auto sub4 = m.GetSubgraph(square4);
+  bool result4 = m.CycleDetection(sub4, square4);
+  EXPECT_EQ(result4, false);
+}
+
+// Test TopologicalSort function
+TEST(TrojanMapStudentTest, TopologicalSort) {
+  TrojanMap m;
+
+  // Case 1: Input using vector
+  std::vector<std::string> location_names = {"Ralphs", "Chick-fil-A", "KFC","Target"};
+  std::vector<std::vector<std::string>> dependencies = {{"Ralphs","KFC"}, {"Ralphs","Chick-fil-A"}, {"KFC","Chick-fil-A"},{"Target","Ralphs"}};
+  auto result = m.DeliveringTrojan(location_names, dependencies);
+  std::vector<std::string> gt ={"Target", "Ralphs","KFC","Chick-fil-A"};
+  EXPECT_EQ(result, gt);
+
+  // Case 2: Read input from csv file
+  std::string dependencies_filename = "/Users/divya/Documents/final-project-Divya-Nandlal-Sahetya/input/topologicalsort_dependencies_2.csv";
+  std::string locations_filename = "/Users/divya/Documents/final-project-Divya-Nandlal-Sahetya/input/topologicalsort_locations_2.csv";
+  location_names = m.ReadLocationsFromCSVFile(locations_filename);
+  dependencies = m.ReadDependenciesFromCSVFile(dependencies_filename);
+  result = m.DeliveringTrojan(location_names, dependencies);
+  gt ={"Shell", "Starbucks","Target","Ralphs","Chick-fil-A","Chipotle","KFC"};
+  EXPECT_EQ(result, gt);
 }
